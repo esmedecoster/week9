fa1cc660dbd90bc97fe12f3928052ea1240d8b33
Esme DeCoster <esmedecoster@seattleacademt.org>
2016-02-11 08:24:56 -0800

    solve uno

---
+++ 01_esme.js
@@ -0,0 +1,9 @@
+//Write a program that takes one argument
+//It should say "hello" and the argument
+//node 01_yourname.js world should output "Hello world"
+
+secondWord = process.argv[2];
+thirdWord = process.argv [3];
+console.log ('Hello ' + secondWord + ' ' +thirdWord);
+
+console.log (process.argv);
